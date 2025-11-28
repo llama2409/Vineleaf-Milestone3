05643762f845eec70dba232687751de9174b348d
llama2409 <nathancornwall2005@icloud.com>
2025-11-28 17:04:41 +0000

    Create home.html and connect it to views.py

--- vine_leaf/booking/views.py
+++ vine_leaf/booking/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to The Vine Leaf Booking System!")
+    return render(request, 'booking/home.html')
