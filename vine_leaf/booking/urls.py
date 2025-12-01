114562947f32692bb962fee10410f856f53bbaa9
llama2409 <nathancornwall2005@icloud.com>
2025-12-01 19:52:28 +0000

    Create booking.html with a form to later integrate Django

--- vine_leaf/booking/urls.py
+++ vine_leaf/booking/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="booking-home"),
+    path("", views.home, name="home"),
+    path('booking/', views.booking_page, name='booking'),
 ]
--- vine_leaf/booking/views.py
+++ vine_leaf/booking/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def booking_page(request):
+    return render(request, 'booking.html')
