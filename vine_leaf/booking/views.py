4c5aa2a0fd5833a0cef416759f30d332e02ee6ca
llama2409 <nathancornwall2005@icloud.com>
2025-12-13 22:01:16 +0000

    Change opening times to match form

--- vine_leaf/booking/views.py
+++ vine_leaf/booking/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from .forms import BookingForm
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def booking_page(request):
     if request.method == 'POST':
@@ -15,11 +18,14 @@ def booking_page(request):
         form = BookingForm()
     return render(request, 'booking.html', {'form': form})
 
+
 def booking_success(request):
     return render(request, 'booking_success.html')
 
+
 def menus_page(request):
     return render(request, 'menus.html')
+
 
 def contact_page(request):
     return render(request, 'contact.html')
