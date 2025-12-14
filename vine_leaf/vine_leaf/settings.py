b523aa47de8631d3fef77e87825616e511a1b331
llama2409 <nathancornwall2005@icloud.com>
2025-12-14 20:04:17 +0000

    Fix static files for Heroku

--- vine_leaf/vine_leaf/settings.py
+++ vine_leaf/vine_leaf/settings.py
@@ -123,10 +123,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
